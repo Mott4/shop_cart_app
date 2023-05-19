b71695220465af67ceba308528dedb524d71bffa
Vitor Mota <dailyslokoscontatoofc@gmail.com>
2023-05-19 12:39:40 -0300

    feat:(Models): Application Models files

---
+++ lib/app/model/cart_item_model.dart
@@ -0,0 +1,8 @@
+import 'package:shop_cart_app/app/model/item_model.dart';
+
+class CartItem {
+  final ItemModel item;
+  final int quantity;
+
+  CartItem(this.item, this.quantity);
+}
---
+++ lib/app/model/item_model.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  final String name;
+  final double price;
+//  final int quantity;
+
+  ItemModel({
+    required this.name,
+    required this.price,
+    // required this.quantity,
+  });
+}
